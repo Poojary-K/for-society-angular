09e3571eb54729f722ddc73c018179b573a46e4e
Rakshith <kotianr002@gmail.com>
2026-06-22 16:32:07 +0530

    Update environment configuration to comment out old API URLs and set the new production API base URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://dev.ird.mu-sigma.com/backendForSociety/api',
+  // apiBaseUrl: 'https://dev.ird.mu-sigma.com/backendForSociety/api',
   // apiBaseUrl: 'http://172.25.10.114:4000/api',
   // apiBaseUrl: 'http://localhost:4000/api',
+  apiBaseUrl: 'https://backend-smdj.onrender.com',
   donationCycleStartDay: 20
 };
