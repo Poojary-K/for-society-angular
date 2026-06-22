5a004e07e0d7ccd8ee06547dee0a42cbeabf9ca2
Rakshith <kotianr002@gmail.com>
2026-06-22 16:48:29 +0530

    env update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   // apiBaseUrl: 'https://dev.ird.mu-sigma.com/backendForSociety/api',
   // apiBaseUrl: 'http://172.25.10.114:4000/api',
   // apiBaseUrl: 'http://localhost:4000/api',
-  apiBaseUrl: 'https://backend-smdj.onrender.com',
+  // apiBaseUrl: 'https://backend-smdj.onrender.com',
+  apiBaseUrl: 'https://forsociety-latest.onrender.com',
   donationCycleStartDay: 20
 };
